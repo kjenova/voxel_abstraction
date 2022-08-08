267d44d5e096eb049ed00ceeb1eb275cc7b2361d
Klemen Enova <klemen.jan.enova@gmail.com>
2022-08-08 19:31:39 +0200

    Othe quaternion tests

--- quat_utils.py
+++ quat_utils.py
@@ -42,16 +42,41 @@ if __name__ == "__main__":
     from pyquaternion import Quaternion
     import numpy as np
 
-    def get_random_quat():
+    def get_random_quat(b = 1, p = 1):
         q = Quaternion.random()
         quat = torch.Tensor(q.elements).float().view(1, 1, 4)
-        return quat, q
+        return quat.repeat(b, p, 1), q
+
+    def conjugate_test_helper(prod):
+        dot_p = prod[..., 0] - prod[..., 1:4].sum(-1)
+        return (1 - dot_p.mean()).abs().item() < 1e-4
 
     def test_quat_conjugate():
-        quat, _ = get_random_quat()
+        quat, _ = get_random_quat(10, 3)
         conj = quat_conjugate(quat)
-        dot_p = quat[..., 0] * conj[..., 0] - (quat[..., 1:4] * conj[..., 1:4]).sum(-1)
-        assert (1 - dot_p.mean()).abs().item() < 1e-4, 'Conjugate is incorrect'
+        assert conjugate_test_helper(quat * conj), 'Conjugate is incorrect'
+        prod = hamilton_product(quat, conj)
+        assert conjugate_test_helper(prod), 'Conjugate/Hamilton product is incorrect'
+
+    def test_hamilton_product():
+        quat1, q1 = get_random_quat()
+        quat2, q2 = get_random_quat()
+        quat_prod = hamilton_product(quat1, quat2).squeeze()
+        q_prod = torch.Tensor((q1 * q2).elements).float()
+        assert (quat_prod - q_prod).abs().mean() < 1e-4, 'Hamilton product is incorrect'
+
+    def test_rotate():
+        points = torch.empty(10, 3, 4, 3).uniform_(-1, 1)
+        quat, _ = get_random_quat(10, 3)
+        points_2 = quat_inverse_rotate(quat_rotate(points, quat), quat)
+        assert (points_2 - points).abs().mean() < 1e-4, 'Rotation/Inverse rotation is incorrect'
+
+        points = torch.empty(1, 1, 1, 3).uniform_(-1, 1)
+        quat, q = get_random_quat()
+        quat_rotated = quat_rotate(points, quat).squeeze()
+        q_rotated = torch.Tensor(q.rotate(points.squeeze().numpy())).float()
+        assert (quat_rotated - q_rotated).abs().mean() < 1e-4, 'Rotation is incorrect'
 
     test_quat_conjugate()
-
+    test_hamilton_product()
+    test_rotate()
