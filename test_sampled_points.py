cf38be0cbfa6098984e52816ea41fc2313b10a86
Klemen Enova <klemen.jan.enova@gmail.com>
2022-09-19 21:22:26 +0200

    Test ujemanja vokselskega volumna s točkami, vzorčenimi z oblike

---
+++ test_sampled_points.py
@@ -0,0 +1,15 @@
+import numpy as np
+import open3d as o3d
+from load_shapenet import load_shapenet, ShapeNetShape
+from write_mesh import write_volume_mesh, write_predictions_mesh
+
+shapenet_dir = 'shapenet/chamferData/03001627'
+n_examples = 5
+dataset = load_shapenet(shapenet_dir)[:n_examples]
+
+for i, shape in enumerate(dataset):
+    write_volume_mesh(shape, i + 1)
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(shape.shape_points)
+    o3d.io.write_point_cloud(f"{i + 1}.ply", pcd)
