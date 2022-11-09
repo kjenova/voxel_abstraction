e7a3d3e6538fec362a02f61c4c67452d174ce7de
klemenjan <ke0306@student.uni-lj.si>
2022-11-09 23:18:39 +0100

    ...

--- miscellaneous/other_tests.py
+++ miscellaneous/other_tests.py
@@ -1,14 +1,11 @@
 import numpy as np
 import open3d as o3d
-from load_shapenet import load_shapenet, ShapeNetShape
-from write_mesh import write_volume_mesh
-from load_shapes import closest_points_grid
-from train import BatchProvider
-from load_shapes import VolumeFaces
 
-shapenet_dir = 'shapenet/chamferData/00'
-max_n_examples = 5
-dataset = load_shapenet(shapenet_dir, max_n_examples)
+from loader.load_preprocessed import load_preprocessed
+from graphics.write_mesh import write_volume_mesh
+
+shapenet_dir = 'data/chamferData/01'
+dataset = load_preprocessed(shapenet_dir, 5)
 
 def to_xzy(m):
     t = str(type(m))
@@ -32,12 +29,11 @@ def plot():
     for i, shape in enumerate(dataset):
         write_shape(shape, i + 1)
 
-
 def index_to_coordinate(i, n):
     return -.5 + (i + .5) / n
 
 def test_closest_points():
-    n = 32
+    n = 64
     for shape in dataset:
         for i in range(n):
             for j in range(n):
@@ -47,27 +43,8 @@ def test_closest_points():
                     z = index_to_coordinate(k, n)
                     u = np.asarray([x, y, z])
 
-                    d1 = (shape.closest_points - u).pow(2).sum(-1).min()
-                    d2 = (shape.closest_points[i, j, k] - u).pow(2).sum(-1)
+                    d1 = ((shape.closest_points - u) ** 2).sum(-1).min() # (shape.closest_points - u).pow(2).sum(-1).min()
+                    d2 = ((shape.closest_points[i, j, k] - u) ** 2).sum(-1) # (shape.closest_points[i, j, k] - u).pow(2).sum(-1)
                     assert np.allclose(d1, d2), 'closest_points incorrect'
-
-class Shape:
-    def __init__(self, volume, sampled_points, closest_points):
-        self.volume = volume
-        self.shape_points = sampled_points
-        self.closest_points = closest_points
-        self.volume_faces = None
-        self.resized_volume_faces = VolumeFaces(volume)
-
-def test_batches():
-    batch_provider = BatchProvider(dataset)
-    (volume, sampled_points, closest_points) = batch_provider.get()
-    volume = volume.cpu().numpy()
-    sampled_points = sampled_points.cpu().numpy()
-    closest_points = closest_points.cpu().numpy()
-
-    for i in range(volume.shape[0]):
-        shape = Shape(volume[i], sampled_points[i], closest_points[i])
-        write_shape(shape, f'b{i + 1}')
 
 plot()
