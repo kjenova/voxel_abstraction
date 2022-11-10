7bbb69589e59bb03d85919dfccea6d707dd09154
klemenjan <ke0306@student.uni-lj.si>
2022-11-10 17:55:28 +0100

    ..

--- common/reconstruction_loss.py
+++ common/reconstruction_loss.py
@@ -39,15 +39,8 @@ def _consistency(volume, P, closest_points_grid, n_samples_per_primitive, expect
     primitive_points = primitive_to_world_space(primitive_points, P.quat, P.trans)
 
     weights = sampler.get_importance_weights(P.dims)
-    # weights *= (P.prob if expected_value else P.exist).unsqueeze(-1)
-    # weights /= weights.sum((1, 2), keepdim = True) + 1e-7
-
-    if expected_value:
-        weights /= P.dims.size(1) * weights.sum(-1, keepdim = True) + 1e-7
-        weights *= P.prob.unsqueeze(-1)
-    else:
-        weights *= P.exist.unsqueeze(-1)
-        weights /= weights.sum((1, 2), keepdim = True) + 1e-7
+    weights *= (P.prob if expected_value else P.exist).unsqueeze(-1)
+    weights /= weights.sum((1, 2), keepdim = True) + 1e-7
 
     i = point_indices(primitive_points, volume)
     closest_points = closest_points_grid.reshape(-1, 3)[i]
@@ -88,9 +81,7 @@ def paschalidou_reconstruction_loss(volume, P, shape_points, closest_points_grid
 
     cov = (sorted_distance * minprob).sum(-1).mean(1)
 
-    # print((cov - coverage(P, shape_points)).abs().mean())
-
-    cons = consistency(volume, P, closest_points_grid, params.n_samples_per_primitive, False)
+    cons = consistency(volume, P, closest_points_grid, params.n_samples_per_primitive, True)
 
     return cov, cons
 
