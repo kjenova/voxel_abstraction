6d1ced09310a89383189712d34e75f79573a82a2
Klemen Enova <klemen.jan.enova@gmail.com>
2022-11-28 20:25:59 +0100

    paschalidou

---
+++ paschalidou/scripts/forward_pass.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+"""Script used to perform a forward pass using a previously trained model and
+visualize the corresponding primitives
+"""
+import argparse
+import os
+import sys
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from arguments import add_voxelizer_parameters, add_nn_parameters, \
+     add_dataset_parameters, add_gaussian_noise_layer_parameters, \
+     voxelizer_shape, add_loss_options_parameters, add_loss_parameters
+from utils import get_colors, store_primitive_parameters
+from visualization_utils import points_on_sq_surface, points_on_cuboid, \
+    save_prediction_as_ply
+
+from learnable_primitives.common.dataset import get_dataset_type,\
+    compose_transformations
+from learnable_primitives.common.model_factory import DatasetBuilder
+from learnable_primitives.equal_distance_sampler_sq import\
+    EqualDistanceSamplerSQ
+from learnable_primitives.models import NetworkParameters
+from learnable_primitives.loss_functions import euclidean_dual_loss
+from learnable_primitives.primitives import\
+    euler_angles_to_rotation_matrices, quaternions_to_rotation_matrices
+from learnable_primitives.voxelizers import VoxelizerFactory
+
+from mayavi import mlab
+
+
+def get_shape_configuration(use_cuboids):
+    if use_cuboids:
+        return points_on_cuboid
+    else:
+        return points_on_sq_surface
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="Do the forward pass and estimate a set of primitives"
+    )
+    parser.add_argument(
+        "dataset_directory",
+        help="Path to the directory containing the dataset"
+    )
+    parser.add_argument(
+        "output_directory",
+        help="Save the output files in that directory"
+    )
+    parser.add_argument(
+        "--tsdf_directory",
+        default="",
+        help="Path to the directory containing the precomputed tsdf files"
+    )
+    parser.add_argument(
+        "--weight_file",
+        default=None,
+        help="The path to the previously trainined model to be used"
+    )
+
+    parser.add_argument(
+        "--n_primitives",
+        type=int,
+        default=32,
+        help="Number of primitives"
+    )
+    parser.add_argument(
+        "--prob_threshold",
+        type=float,
+        default=0.5,
+        help="Probability threshold"
+    )
+    parser.add_argument(
+        "--use_deformations",
+        action="store_true",
+        help="Use Superquadrics with deformations as the shape configuration"
+    )
+    parser.add_argument(
+        "--save_prediction_as_mesh",
+        action="store_true",
+        help="When true store prediction as a mesh"
+    )
+    parser.add_argument(
+        "--run_on_gpu",
+        action="store_true",
+        help="Use GPU"
+    )
+    parser.add_argument(
+        "--with_animation",
+        action="store_true",
+        help="Add animation"
+    )
+
+    add_dataset_parameters(parser)
+    add_nn_parameters(parser)
+    add_voxelizer_parameters(parser)
+    add_gaussian_noise_layer_parameters(parser)
+    add_loss_parameters(parser)
+    add_loss_options_parameters(parser)
+    args = parser.parse_args(argv)
+
+    # A sampler instance
+    e = EqualDistanceSamplerSQ(200)
+
+    # Check if output directory exists and if it doesn't create it
+    if not os.path.exists(args.output_directory):
+        os.makedirs(args.output_directory)
+
+    if args.run_on_gpu and torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+    print "Running code on ", device
+
+    # Create a factory that returns the appropriate voxelizer based on the
+    # input argument
+    voxelizer_factory = VoxelizerFactory(
+        args.voxelizer_factory,
+        np.array(voxelizer_shape(args)),
+        args.save_voxels_to
+    )
+
+    # Create a dataset instance to generate the samples for training
+    dataset = get_dataset_type("euclidean_dual_loss")(
+        (DatasetBuilder()
+            .with_dataset(args.dataset_type)
+            .filter_tags(args.model_tags)
+            .build(args.dataset_directory)),
+        voxelizer_factory,
+        args.n_points_from_mesh,
+        transform=compose_transformations(voxelizer_factory)
+    )
+
+    # TODO: Change batch_size in dataloader
+    dataloader = DataLoader(dataset, batch_size=1, num_workers=4)
+
+    network_params = NetworkParameters.from_options(args)
+    # Build the model to be used for testing
+    model = network_params.network(network_params)
+    # Move model to device to be used
+    model.to(device)
+    if args.weight_file is not None:
+        # Load the model parameters of the previously trained model
+        model.load_state_dict(
+            torch.load(args.weight_file, map_location=device)
+        )
+    model.eval()
+
+    colors = get_colors(args.n_primitives)
+    for sample in dataloader:
+        X, y_target = sample
+        X, y_target = X.to(device), y_target.to(device)
+
+        # Do the forward pass and estimate the primitive parameters
+        y_hat = model(X)
+
+        M = args.n_primitives  # number of primitives
+        probs = y_hat[0].to("cpu").detach().numpy()
+        # Transform the Euler angles to rotation matrices
+        if y_hat[2].shape[1] == 3:
+            R = euler_angles_to_rotation_matrices(
+                y_hat[2].view(-1, 3)
+            ).to("cpu").detach()
+        else:
+            R = quaternions_to_rotation_matrices(
+                    y_hat[2].view(-1, 4)
+                ).to("cpu").detach()
+            # get also the raw quaternions
+            quats = y_hat[2].view(-1, 4).to("cpu").detach().numpy()
+        translations = y_hat[1].to("cpu").view(args.n_primitives, 3)
+        translations = translations.detach().numpy()
+
+        shapes = y_hat[3].to("cpu").view(args.n_primitives, 3).detach().numpy()
+        epsilons = y_hat[4].to("cpu").view(
+            args.n_primitives, 2
+        ).detach().numpy()
+        taperings = y_hat[5].to("cpu").view(
+            args.n_primitives, 2
+        ).detach().numpy()
+
+        pts = y_target[:, :, :3].to("cpu")
+        pts_labels = y_target[:, :, -1].to("cpu").squeeze().numpy()
+        pts = pts.squeeze().detach().numpy().T
+
+        on_prims = 0
+        fig = mlab.figure(size=(400, 400), bgcolor=(1, 1, 1))
+        mlab.view(azimuth=0.0, elevation=0.0, distance=2)
+        # Uncomment to visualize the points sampled from the target mesh
+        # t = np.array([1.2, 0, 0]).reshape(3, -1)
+        # pts_n = pts + t
+        #     mlab.points3d(
+        #        # pts_n[0], pts_n[1], pts_n[2],
+        #        pts[0], pts[1], pts[2],
+        #        scale_factor=0.03, color=(0.8, 0.8, 0.8)
+        #     )
+
+        # Keep track of the files containing the parameters of each primitive
+        primitive_files = []
+        for i in range(args.n_primitives):
+            x_tr, y_tr, z_tr, prim_pts =\
+                get_shape_configuration(args.use_cuboids)(
+                    shapes[i, 0],
+                    shapes[i, 1],
+                    shapes[i, 2],
+                    epsilons[i, 0],
+                    epsilons[i, 1],
+                    R[i].numpy(),
+                    translations[i].reshape(-1, 1),
+                    taperings[i, 0],
+                    taperings[i, 1]
+                )
+
+            # Dump the parameters of each primitive as a dictionary
+            store_primitive_parameters(
+                size=tuple(shapes[i]),
+                shape=tuple(epsilons[i]),
+                rotation=tuple(quats[i]),
+                location=tuple(translations[i]),
+                tapering=tuple(taperings[i]),
+                probability=(probs[0, i],),
+                color=(colors[i % len(colors)]) + (1.0,),
+                filepath=os.path.join(
+                    args.output_directory,
+                    "primitive_%d.p" %(i,)
+                )
+            )
+            if probs[0, i] >= args.prob_threshold:
+                on_prims += 1
+                mlab.mesh(
+                    x_tr,
+                    y_tr,
+                    z_tr,
+                    color=tuple(colors[i % len(colors)]),
+                    opacity=1.0
+                )
+                primitive_files.append(
+                    os.path.join(args.output_directory, "primitive_%d.p" % (i,))
+                )
+
+        if args.with_animation:
+            cnt = 0
+            for az in range(0, 360, 1):
+                cnt += 1
+                mlab.view(azimuth=az, elevation=0.0, distance=2)
+                mlab.savefig(
+                    os.path.join(
+                        args.output_directory,
+                        "img_%04d.png" % (cnt,)
+                    )
+                )
+        for i in range(args.n_primitives):
+            print i, probs[0, i]
+
+        print "Using %d primitives out of %d" % (on_prims, args.n_primitives)
+        mlab.show()
+
+        if args.save_prediction_as_mesh:
+            print "Saving prediction as mesh...."
+            save_prediction_as_ply(
+                primitive_files,
+                os.path.join(args.output_directory, "primitives.ply")
+            )
+            print "Saved prediction as ply file in {}".format(
+                os.path.join(args.output_directory, "primitives.ply")
+            )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ train_paschalidou.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+"""Script used to train the network for representing a 3D object as a set of
+primitives
+"""
+import argparse
+from itertools import izip
+import json
+import random
+import os
+import string
+import sys
+
+import numpy as np
+import torch
+
+from paschalidou.scripts.arguments import add_voxelizer_parameters, add_nn_parameters,\
+    add_dataset_parameters, add_training_parameters,\
+    add_regularizer_parameters, add_sq_mesh_sampler_parameters,\
+    add_gaussian_noise_layer_parameters, voxelizer_shape,\
+    add_loss_options_parameters, add_loss_parameters, get_loss_options
+from paschalidou.scripts.output_logger import get_logger
+
+from paschalidou.learnable_primitives.equal_distance_sampler_sq import get_sampler
+from paschalidou.learnable_primitives.models import NetworkParameters, train_on_batch, \
+    optimizer_factory
+from paschalidou.learnable_primitives.loss_functions import euclidean_dual_loss
+
+from common.batch_provider import BatchProvider, BatchProviderParams
+
+from loader.load_preprocessed import load_preprocessed
+from loader.load_urocell import load_urocell_preprocessed
+
+def moving_average(prev_val, new_val, b):
+    return (prev_val*b + new_val) / (b+1)
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
+
+
+def lr_schedule(optimizer, current_epoch, init_lr, factor, reductions):
+    def inner(epoch):
+        for i, e in enumerate(reductions):
+            if epoch < e:
+                return init_lr*factor**(-i)
+        return init_lr*factor**(-len(reductions))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = inner(current_epoch)
+
+    return optimizer
+
+
+def get_weight(w, epoch, current_epoch):
+    if current_epoch < epoch:
+        return 0.0
+    else:
+        return w
+
+
+def get_regularizer_terms(args, current_epoch):
+    # Create a dictionary with the regularization terms if there are any
+    regularizer_terms = {
+        "regularizer_type": args.regularizer_type,
+        "bernoulli_regularizer_weight": args.bernoulli_regularizer_weight,
+        "entropy_bernoulli_regularizer_weight":
+            args.entropy_bernoulli_regularizer_weight,
+        "parsimony_regularizer_weight": args.parsimony_regularizer_weight,
+        "sparsity_regularizer_weight": args.sparsity_regularizer_weight,
+        "overlapping_regularizer_weight": args.overlapping_regularizer_weight,
+        "minimum_number_of_primitives": args.minimum_number_of_primitives,
+        "maximum_number_of_primitives": args.maximum_number_of_primitives,
+        "w1": args.w1,
+        "w2": args.w2
+    }
+
+    return regularizer_terms
+
+
+def save_experiment_params(args, experiment_tag, directory):
+    t = vars(args)
+    params = {k: str(v) for k, v in t.iteritems()}
+
+    git_head_hash = "foo"
+    params["git-commit"] = git_head_hash
+    params["experiment_tag"] = experiment_tag
+    for k, v in params.items():
+        if v == "":
+            params[k] = None
+    with open(os.path.join(directory, "params.json"), "w") as f:
+        json.dump(params, f, indent=4)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="Train a network to predict primitives"
+    )
+    parser.add_argument(
+        "train_dir",
+        default="data/chamferData/01",
+        help="Path to the directory containing the dataset"
+    )
+    parser.add_argument(
+        "train_dir",
+        default="data/chamferData/01",
+        help="Path to the directory containing the dataset"
+    )
+    parser.add_argument(
+        "urocell_dir",
+        default="data/chamferData/urocell"
+    )
+    parser.add_argument(
+        "save_dir",
+        default="results/paschalidou",
+        help="Save the output files in that directory"
+    )
+
+    parser.add_argument(
+        "--weight_file",
+        default=None,
+        help=("The path to a previously trainined model to continue"
+              " the training from")
+    )
+    parser.add_argument(
+        "--continue_from_epoch",
+        default=0,
+        type=int,
+        help="Continue training from epoch (default=0)"
+    )
+    parser.add_argument(
+        "--n_primitives",
+        type=int,
+        default=32,
+        help="Number of primitives"
+    )
+    parser.add_argument(
+        "--use_deformations",
+        action="store_true",
+        help="Use Superquadrics with deformations as the shape configuration"
+    )
+    parser.add_argument(
+        "--probs_only",
+        action="store_true",
+        help="Optimize only using the probabilities"
+    )
+
+    parser.add_argument(
+        "--experiment_tag",
+        default=None,
+        help="Tag that refers to the current experiment"
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=27,
+        help="Seed for the PRNG"
+    )
+
+    add_nn_parameters(parser)
+    add_dataset_parameters(parser)
+    add_voxelizer_parameters(parser)
+    add_training_parameters(parser)
+    add_sq_mesh_sampler_parameters(parser)
+    add_regularizer_parameters(parser)
+    add_gaussian_noise_layer_parameters(parser)
+    # Parameters related to the loss function and the loss weights
+    add_loss_parameters(parser)
+    # Parameters related to loss options
+    add_loss_options_parameters(parser)
+    args = parser.parse_args(argv)
+
+    device = torch.device("cuda:1")
+
+    # Check if output directory exists and if it doesn't create it
+    if not os.path.exists(args.output_directory):
+        os.makedirs(args.output_directory)
+
+    # Create an experiment directory using the experiment_tag
+    if args.experiment_tag is None:
+        experiment_tag = id_generator(9)
+    else:
+        experiment_tag = args.experiment_tag
+
+    experiment_directory = os.path.join(
+        args.output_directory,
+        experiment_tag
+    )
+    if not os.path.exists(experiment_directory):
+        os.makedirs(experiment_directory)
+
+    # Store the parameters for the current experiment in a json file
+    save_experiment_params(args, experiment_tag, experiment_directory)
+    print "Save experiment statistics in %s" %(experiment_tag, )
+
+    # Create two files to store the training and test evolution
+    train_stats = os.path.join(experiment_directory, "train.txt")
+    val_stats = os.path.join(experiment_directory, "val.txt")
+    if args.weight_file is None:
+        train_stats_f = open(train_stats, "w")
+    else:
+        train_stats_f = open(train_stats, "a+")
+    train_stats_f.write((
+        "epoch loss pcl_to_prim_loss prim_to_pcl_loss bernoulli_regularizer "
+        "entropy_bernoulli_regularizer parsimony_regularizer "
+        "overlapping_regularizer sparsity_regularizer\n"
+    ))
+
+    # Set the random seed
+    np.random.seed(args.seed)
+    torch.manual_seed(np.random.randint(np.iinfo(np.int32).max))
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(np.random.randint(np.iinfo(np.int32).max))
+
+    # Create an object that will sample points in equal distances on the
+    # surface of the primitive
+    sampler = get_sampler(
+        args.use_cuboids,
+        args.n_points_from_sq_mesh,
+        args.D_eta,
+        args.D_omega
+    )
+
+    train_set = load_preprocessed(args.train_dir)
+    validation_set, _ = load_urocell_preprocessed(args.urocell_dir)
+
+    batch_params = BatchProviderParams(
+        device,
+        n_samples_per_shape = args.n_points_from_mesh,
+        batch_size = args.batch_size
+    )
+
+    train_batches = BatchProvider(
+        train_set,
+        batch_params,
+        store_on_gpu = False,
+        uses_point_sampling = True
+    )
+    validation_batches = BatchProvider(
+        validation_set,
+        batch_params,
+        store_on_gpu = False,
+        uses_point_sampling = True
+    )
+
+    network_params = NetworkParameters.from_options(args)
+    # Build the model to be used for training
+    model = network_params.network(network_params)
+
+    # Move model to the device to be used
+    model.to(device)
+    # Check whether there is a weight file provided to continue training from
+    if args.weight_file is not None:
+        model.load_state_dict(torch.load(args.weight_file))
+    model.train()
+
+    # Build an optimizer object to compute the gradients of the parameters
+    optimizer = optimizer_factory(args, model)
+
+    # Loop over the dataset multiple times
+    pcl_to_prim_losses = []
+    prim_to_pcl_losses = []
+    losses = []
+    for i in range(args.epochs):
+        bar = get_logger(
+            "euclidean_dual_loss",
+            i+1,
+            args.epochs,
+            args.steps_per_epoch
+        )
+        for b in range(args.steps_per_epoch):
+            X, y_target = train_batches.get()[:2]
+
+            # Train on batch
+            batch_loss, metrics, debug_stats = train_on_batch(
+                model,
+                lr_schedule(
+                    optimizer, i, args.lr, args.lr_factor, args.lr_epochs
+                ),
+                euclidean_dual_loss,
+                X,
+                y_target,
+                get_regularizer_terms(args, i),
+                sampler,
+                get_loss_options(args)
+            )
+
+            # Get the regularizer terms
+            reg_values = debug_stats["regularizer_terms"]
+            sparsity_regularizer = reg_values["sparsity_regularizer"]
+            overlapping_regularizer = reg_values["overlapping_regularizer"]
+            parsimony_regularizer = reg_values["parsimony_regularizer"] 
+            entropy_bernoulli_regularizer = reg_values["entropy_bernoulli_regularizer"]
+            bernoulli_regularizer = reg_values["bernoulli_regularizer"]
+
+            # The lossess
+            pcl_to_prim_loss = debug_stats["pcl_to_prim_loss"].item()
+            prim_to_pcl_loss = debug_stats["prim_to_pcl_loss"].item()
+            bar.loss = moving_average(bar.loss, batch_loss, b)
+            bar.pcl_to_prim_loss = \
+                moving_average(bar.pcl_to_prim_loss, pcl_to_prim_loss, b)
+            bar.prim_to_pcl_loss = \
+                moving_average(bar.prim_to_pcl_loss, prim_to_pcl_loss, b)
+
+            losses.append(bar.loss)
+            prim_to_pcl_losses.append(bar.prim_to_pcl_loss)
+            pcl_to_prim_losses.append(bar.pcl_to_prim_loss)
+
+            bar.bernoulli_regularizer =\
+                (bar.bernoulli_regularizer * b + bernoulli_regularizer) / (b+1)
+            bar.parsimony_regularizer =\
+                (bar.parsimony_regularizer * b + parsimony_regularizer) / (b+1)
+            bar.overlapping_regularizer =\
+                (bar.overlapping_regularizer * b + overlapping_regularizer) / (b+1)
+            bar.entropy_bernoulli_regularizer = \
+                (bar.entropy_bernoulli_regularizer * b +
+                 entropy_bernoulli_regularizer) / (b+1)
+            bar.sparsity_regularizer =\
+                (bar.sparsity_regularizer * b + sparsity_regularizer) / (b+1)
+
+            bar.exp_n_prims = metrics[0].sum(-1).mean()
+            # Update the file that keeps track of the statistics
+            train_stats_f.write(
+                ("%d %.8f %.8f %.8f %.6f %.6f %.6f %.6f %.6f") %(
+                i, bar.loss, bar.pcl_to_prim_loss, bar.prim_to_pcl_loss,
+                bar.bernoulli_regularizer,
+                bar.entropy_bernoulli_regularizer,
+                bar.parsimony_regularizer,
+                bar.overlapping_regularizer,
+                bar.sparsity_regularizer
+                )
+            )
+            train_stats_f.write("\n")
+            train_stats_f.flush()
+
+            bar.next()
+
+        # Finish the progress bar and save the model after every epoch
+        bar.finish()
+
+        torch.save(
+            model.state_dict(),
+            os.path.join(
+                experiment_directory,
+                "model_%d" % (i + args.continue_from_epoch,)
+            )
+        )
+
+    print [
+        sum(losses[args.steps_per_epoch:]) / float(args.steps_per_epoch),
+        sum(losses[:args.steps_per_epoch]) / float(args.steps_per_epoch),
+        sum(pcl_to_prim_losses[args.steps_per_epoch:]) / float(args.steps_per_epoch),
+        sum(pcl_to_prim_losses[:args.steps_per_epoch]) / float(args.steps_per_epoch),
+        sum(prim_to_pcl_losses[args.steps_per_epoch:]) / float(args.steps_per_epoch),
+        sum(prim_to_pcl_losses[:args.steps_per_epoch]) / float(args.steps_per_epoch),
+    ]
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
